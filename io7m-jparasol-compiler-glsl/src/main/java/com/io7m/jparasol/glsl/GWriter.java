0bfb7027c0f52eee874ed2a2ffc392782aa1bfb7
Mark Raynsford <code@io7m.com>
2014-09-04 19:52:56 +0000

    Declare all GLSL function parameters as "in". Implements [a3da0897a2].

--- io7m-jparasol-compiler-glsl/src/main/java/com/io7m/jparasol/glsl/GWriter.java
+++ io7m-jparasol-compiler-glsl/src/main/java/com/io7m/jparasol/glsl/GWriter.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -827,7 +827,7 @@ import com.io7m.junreachable.UnreachableCodeException;
 
   /**
    * Write the given GLSL shader to the given output stream.
-   * 
+   *
    * @param out
    *          The stream
    * @param f
@@ -885,7 +885,7 @@ import com.io7m.junreachable.UnreachableCodeException;
       w.println("(");
       for (int index = 0; index < max; ++index) {
         final Pair<GTermNameLocal, GTypeName> p = parameters.get(index);
-        w.print(String.format("  %s %s", p.getRight().show(), p
+        w.print(String.format("  in %s %s", p.getRight().show(), p
           .getLeft()
           .show()));
         if ((index + 1) < max) {
@@ -1227,7 +1227,7 @@ import com.io7m.junreachable.UnreachableCodeException;
 
   /**
    * Write the given GLSL shader to the given output stream.
-   * 
+   *
    * @param out
    *          The stream
    * @param v
